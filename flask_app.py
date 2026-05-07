f646473afc314a57fe2b1fccc7ee9b43fe47ca00
Raul055 <raul_rr305@outlook.com>
2026-05-06 22:30:14 -0600

    login dummy

--- flask_app.py
+++ flask_app.py
@@ -1,13 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
 from forms import login_form
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '9790e9651675c712d6a295af712cdef4'
 
 @app.route("/")
-@app.route("/login")
+def home():
+    return render_template('home.html')
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = login_form()
+    if form.validate_on_submit():
+        if form.username.data == 'admin' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Check username and password.', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
